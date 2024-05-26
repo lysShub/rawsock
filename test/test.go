21ee531d9aa3bd455f0450e8ce581678dd0846e2
lysShub <lysShub@gmail.com>
2024-05-26 08:14:33 +0800

    optimize ut

--- test/test.go
+++ test/test.go
@@ -470,7 +470,8 @@ func BindRawToUstack(t require.TestingT, ctx context.Context, us *ustack, raw ra
 			// 	tcphdr.Flags(),
 			// )
 
-			err := raw.Write(packet.Make().Append(StripIP(ip)))
+			tcp := StripIP(ip)
+			err := raw.Write(packet.Make(20, 0, len(tcp)).Append(tcp))
 			require.NoError(t, err)
 		}
 	}()
